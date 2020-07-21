228b3a57d839cd3c1de2d671970965cbbd19b933
peawarrior <jacksonchen171@gmail.com>
2020-07-21 13:52:08 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbaity?
+
+    private
+
+    def clickbaity?
+        return if title.blank?
+        if !title.match?(/Won't Believe|Secret|Top \d+/)
+            errors.add(:title, "must contain a click bait")
+        end
+    end
+
 end
