edda9d434108d1dc46425709854a786d12204bc5
21shield <21netaly@gmail.com>
2020-07-23 22:47:04 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  
+    # phonenumber == 10 digits/ also numerically
+    validates :name, :phone_number, presence: true, uniqueness: true
+    validates :phone_number, numericality:{only_integer: true}, length: {is: 10}
+
 end
