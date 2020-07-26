a22531f498e3913aa56d15c7c2b5978c7646d3f4
chrisvsanchez <chrisvsanchez@gmail.com>
2020-07-26 18:35:01 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Scary Stories)}
+    validate :click_bait
+
+    def click_bait
+    end
+
 end
